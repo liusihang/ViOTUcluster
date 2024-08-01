a8003909dff8cc5e469352c338fe7ee460158e3e
shuofang <916931057@qq.com>
2024-08-01 19:43:59 +0800

    Add setup script and config

--- setup.sh
+++ setup.sh
@@ -11,23 +11,17 @@ DB_DIR=$1
 NUM_THREADS=$2
 
 # Create and activate the Conda environment
-# mamba create -n vOTUcluster -c conda-forge -c bioconda "python=3.8" scikit-learn=0.22.1 imbalanced-learn pandas seaborn pyhmmer==0.10.14 prodigal screed ruamel.yaml "snakemake>=5.18,<=5.26" click "conda-package-handling<=1.9"
-# mamba activate vOTUcluster
+mamba env create -f ./setupscript/vOTUcluster.yaml
+mamba activate vOTUcluster
 
 # Clone the repository and install the package
-git clone https://github.com/liusihang/VirSorter2-pyhmmerAcc.git
+git clone https://gitclone.com/github.com/liusihang/VirSorter2-pyhmmerAcc.git
 cd VirSorter2-pyhmmerAcc
 pip install -e .
 
-# Install additional packages
-mamba install -c conda-forge -c bioconda viralverify
-mamba install -c conda-forge -c bioconda geNomad
-mamba install -c conda-forge -c bioconda checkv
-mamba install -c conda-forge -c bioconda drep
-
 # Run Python scripts
-python Move2bin.py
-python ReplaceVf.py
+python ./setupscript/Move2bin.py
+python ./setupscript/ReplaceVf.py
 
 # Setup the database for VirSorter
 virsorter setup -d "$DB_DIR" -j "$NUM_THREADS"
