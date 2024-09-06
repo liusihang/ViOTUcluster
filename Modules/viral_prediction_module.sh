a5f16b8bf7e2f79b45acc41d3f6f16e10972ed5a
shuofang <916931057@qq.com>
2024-09-06 17:04:48 +0800

    Bug fix

--- Modules/viral_prediction_module.sh
+++ Modules/viral_prediction_module.sh
@@ -1,17 +1,17 @@
 #!/usr/bin/env bash
 
-# 设置错误捕获机制：如果发生错误，脚本将停止执行
+# Set error handling mechanism: if an error occurs, the script will stop executing
 set -e
 trap 'echo "An error occurred. Exiting..."; exit 1;' ERR
 
-# 确保 BASE_CONDA_PREFIX 被正确设置
+# Make sure BASE_CONDA_PREFIX is correctly set
 BASE_CONDA_PREFIX=$(conda info --base)
 conda_sh="$BASE_CONDA_PREFIX/etc/profile.d/conda.sh"
 
-# 导出必要的变量和函数
+# Export necessary variables and functions
 export OUTPUT_DIR DATABASE Group CONCENTRATION_TYPE
 
-# 处理文件的主函数
+# Main function to process files
 process_file() {
   local FILE=$1
   local BASENAME=$(basename "$FILE" .fa)
@@ -30,36 +30,44 @@ process_file() {
 
   echo "Processing $FILE"
 
-  # 执行 viralverify 分析
+  # Run viralverify analysis
   if [ ! -f "$Viralverify_dir/${BASENAME}_result_table.csv" ]; then
     echo "Running viralverify prediction..."
     source ${conda_sh}
-    viralverify -f "$FILE" -o "$Viralverify_dir" --hmm "$DATABASE/ViralVerify/nbc_hmms.hmm" -t 104 > "$Viralverify_dir/viralverify.log" 2>&1
-    echo "viralverify prediction completed!"
+    viralverify -f "$FILE" -o "$Viralverify_dir" --hmm "$DATABASE/ViralVerify/nbc_hmms.hmm" -t 104 > "$Viralverify_dir/viralverify.log" 2>&1 &
+    VVERIFY_PID=$!  # Get the process ID of viralverify
   else
     echo "viralverify prediction already completed for $FILE, skipping..."
   fi
 
-  # 仅当是 concentration 时才运行 Virsorter2
+  # Run Virsorter2 only when it is concentration
   if [ "$CONCENTRATION_TYPE" == "concentration" ]; then
     local Virsorter_dir="$PREDICTION_DIR/virsorter2"
     mkdir -p "$Virsorter_dir"
 
     if [ ! -f "$Virsorter_dir/final-viral-score.tsv" ]; then
       echo "Running Virsorter2 prediction..."
-      virsorter run -w "$Virsorter_dir" -i "$FILE" --include-groups "$Group" -j 104 all --min-score 0.5 --min-length 2000 --keep-original-seq -d "$DATABASE/db" > "$Virsorter_dir/virsorter.log" 2>&1
-      echo "Virsorter2 prediction completed!"
+      virsorter run -w "$Virsorter_dir" -i "$FILE" --include-groups "$Group" -j 104 all --min-score 0.5 --min-length 2000 --keep-original-seq -d "$DATABASE/db" > "$Virsorter_dir/virsorter.log" 2>&1 &
+      VSORTER_PID=$!  # Get the process ID of virsorter2
     else
       echo "Virsorter2 prediction already completed for $FILE, skipping..."
     fi
   fi
+
+  # Wait for all background tasks to complete
+  wait $VVERIFY_PID
+  if [ "$CONCENTRATION_TYPE" == "concentration" ]; then
+    wait $VSORTER_PID
+  fi
+
+  echo "All predictions completed for $FILE"
 }
 
 export -f process_file
 parallel process_file ::: $FILES
 
-# 进行 Genomad 分析
-for FILE in $RawFILES; do
+# Perform Genomad analysis
+for FILE in $FILES; do
   echo "Processing $FILE"
   BASENAME=$(basename "$FILE" .fa)
   BASENAME=${BASENAME%.fasta}
@@ -67,20 +75,20 @@ for FILE in $RawFILES; do
   OUT_DIR="$OUTPUT_DIR/SeprateFile/${BASENAME}"
   PREDICTION_DIR="$OUT_DIR/RoughViralPrediction"
 
-  # genomad 预测
-  echo -e "\n \n \n # 进行genomad预测!!! \n \n \n"
+  # Genomad prediction
+  echo -e "\n \n \n # Performing genomad prediction!!! \n \n \n"
   Genomad_dir="$PREDICTION_DIR/genomadres"
   mkdir -p "$Genomad_dir"
 
   if [ ! -f "$Genomad_dir/${BASENAME}_summary/${BASENAME}_virus_summary.tsv" ]; then
     genomad end-to-end --enable-score-calibration "$FILE" "$Genomad_dir" "$DATABASE/genomad_db"
-    echo -e "\n \n \n # genomad预测完成!!! \n \n \n"
+    echo -e "\n \n \n # Genomad prediction completed!!! \n \n \n"
   else
     echo "genomad prediction already completed for $FILE, skipping..."
   fi
 done
 
-# 检测所有 Virsorter2 任务是否完成 (仅当 concentration 时)
+# Check if all Virsorter2 tasks are completed (only when it is concentration)
 if [ "$CONCENTRATION_TYPE" == "concentration" ]; then
   all_tasks_completed=false
   while [ "$all_tasks_completed" == "false" ]; do
