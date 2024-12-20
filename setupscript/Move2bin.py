802d3a98405a80ea78d9eeb8f7544b32eca04dfc
shuofang <916931057@qq.com>
2024-12-20 17:48:20 +0800

    Bug fixed

--- setupscript/Move2bin.py
+++ setupscript/Move2bin.py
@@ -3,14 +3,14 @@ import shutil
 import stat
 
 def replace_and_copy_scripts():
-    # 获取当前Conda环境的目录
+    # Get the current Conda environment directory
     conda_prefix = os.environ.get('CONDA_PREFIX')
     
     if not conda_prefix:
         print("Error: No Conda environment is currently active.")
         return
 
-    # 确定源目录和目标目录
+    # Define source and target directories
     source_dir = os.path.join(os.getcwd(), 'bin')
     target_dir = os.path.join(conda_prefix, 'bin')
 
@@ -21,7 +21,7 @@ def replace_and_copy_scripts():
     if not os.path.exists(target_dir):
         os.makedirs(target_dir)
 
-    # 复制和替换viralverify文件
+    # Copy and replace viralverify file
     source_viralverify = os.path.join(source_dir, 'viralverify')
     if not os.path.exists(source_viralverify):
         print(f"Error: Source file '{source_viralverify}' does not exist.")
@@ -32,32 +32,32 @@ def replace_and_copy_scripts():
             if file == 'viralverify':
                 target_file = os.path.join(root, file)
                 shutil.copy(source_viralverify, target_file)
-                # 设置可执行权限
+                # Set executable permissions
                 st = os.stat(target_file)
                 os.chmod(target_file, st.st_mode | stat.S_IEXEC)
                 print(f"Replaced and made executable {target_file} with {source_viralverify}")
 
-    # 复制Bin脚本文件到目标目录并设置可执行权限
+    # Copy bin scripts to target directory and set executable permissions
     for filename in os.listdir(source_dir):
         source_file = os.path.join(source_dir, filename)
         target_file = os.path.join(target_dir, filename)
 
         if os.path.isfile(source_file):
             shutil.copy(source_file, target_file)
-            # 设置可执行权限
+            # Set executable permissions
             st = os.stat(target_file)
             os.chmod(target_file, st.st_mode | stat.S_IEXEC)
             print(f"Copied and made executable {source_file} to {target_file}")
 
     source_dir = os.path.join(os.getcwd(), 'Modules')
-    # 复制Source脚本文件到目标目录并设置可执行权限
+    # Copy module scripts to target directory and set executable permissions
     for filename in os.listdir(source_dir):
         source_file = os.path.join(source_dir, filename)
         target_file = os.path.join(target_dir, filename)
 
         if os.path.isfile(source_file):
             shutil.copy(source_file, target_file)
-            # 设置可执行权限
+            # Set executable permissions
             st = os.stat(target_file)
             os.chmod(target_file, st.st_mode | stat.S_IEXEC)
             print(f"Copied and made executable {source_file} to {target_file}")
