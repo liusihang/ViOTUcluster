19f997046d34d01b87993e4a734e729fe1fd4a10
shuofang <916931057@qq.com>
2025-03-07 14:42:18 +0800

    Bug fixed

--- Modules/drep_module.sh
+++ Modules/drep_module.sh
@@ -37,6 +37,11 @@ else
 
     newDir="$OUTPUT_DIR/Summary/temp"
     
+    echo "Filtering sequences shorter than 3000bp..."
+    awk 'BEGIN {RS=">";FS="\n"} NR>1 {seq=""; for(i=2;i<=NF;i++) seq=seq $i; if(length(seq)>=3000) print ">" $1 "\n" seq}' \
+        "${newDir}/merged_sequences.fasta" > "${newDir}/merged_sequences_filtered.fasta"
+    mv "${newDir}/merged_sequences_filtered.fasta" "${newDir}/merged_sequences.fasta"
+    
     echo "Clustering..."
     makeblastdb -in "${newDir}/merged_sequences.fasta" -dbtype nucl -out "${newDir}/temp_db"
 
